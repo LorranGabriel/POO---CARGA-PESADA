19e7d26eaa4a9ef29d4f693de60bb787544b26b5
Leonardo Voltolin De Sena <20181BSI0172@cefetes.br>
2018-11-05 09:33:47 -0200

    refatoracao

---
+++ CargaPesada/src/modelo/Cliente.java
@@ -0,0 +1,71 @@
+package modelo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author 20181bsi0172
+ */
+public class Cliente
+    {
+        private int id_cliente;
+        private String nome;
+        private long cnpj;
+        private long cpf;
+        private Endereco endereco;
+        private Contato contato;
+    
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(long cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Contato getContato() {
+        return contato;
+    }
+
+    public void setContato(Contato contato) {
+        this.contato = contato;
+    }
+      
+}
---
+++ CargaPesada/src/modelo/Veiculo.java
@@ -0,0 +1,116 @@
+package modelo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author 20181bsi0172
+ */
+public class Veiculo {
+
+    private int id_veiculo;
+    private String chassi;
+    private String placa;
+    private String status;
+    private String tipoCombustivel;
+    private String categoria;
+    private Seguro seguro;
+    private Modelo modelo;
+    private Motorista motorista;
+    private Financiamento financiamento;
+
+    public Veiculo(String chassi, String placa, String status, String tipoCombustivel, String categoria, Modelo modelo, Motorista motorista, Financiamento financiamento) {
+        this.chassi = chassi;
+        this.placa = placa;
+        this.status = status;
+        this.tipoCombustivel = tipoCombustivel;
+        this.categoria = categoria;
+        //this.seguro = seguro;
+        this.modelo = modelo;
+        this.motorista = motorista;
+        this.financiamento = financiamento;
+    }
+
+    public int getId_veiculo() {
+        return id_veiculo;
+    }
+
+    public String getChassi() {
+        return chassi;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public Seguro getSeguro() {
+        return seguro;
+    }
+
+    public Financiamento getFinanciamento() {
+        return financiamento;
+    }
+
+    public Modelo getModelo() {
+        return modelo;
+    }
+
+    public Motorista getMotorista() {
+        return motorista;
+    }
+
+    public void setModelo(Modelo modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setMotorista(Motorista motorista) {
+        this.motorista = motorista;
+    }
+
+    public void setFinanciamento(Financiamento financiamento) {
+        this.financiamento = financiamento;
+    }
+
+    public void setSeguro(Seguro seguro) {
+        this.seguro = seguro;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setId_veiculo(int id_veiculo) {
+        this.id_veiculo = id_veiculo;
+    }
+
+    public void setChassi(String chassi) {
+        this.chassi = chassi;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setTipoCombustivel(String tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+    }
+}
